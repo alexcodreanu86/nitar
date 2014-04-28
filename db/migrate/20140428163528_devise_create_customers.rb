29d0c1f060eeb517db9cfd835098a29d1717377e
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-04-28 11:36:03 -0500

    generated devise customer

--- db/migrate/20140428163528_devise_create_customers.rb
+++ db/migrate/20140428163528_devise_create_customers.rb
@@ -1,13 +1,13 @@
-class DeviseCreateUsers < ActiveRecord::Migration
+class DeviseCreateCustomers < ActiveRecord::Migration
   def change
-    create_table(:users) do |t|
+    create_table(:customers) do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       # Custome fields
-      t.string :first_name, null: false
-      t.string :last_name, null: false
+      t.string :first_name
+      t.string :last_name
 
       ## Recoverable
       t.string   :reset_password_token
@@ -38,9 +38,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :customers, :email,                unique: true
+    add_index :customers, :reset_password_token, unique: true
+    # add_index :customers, :confirmation_token,   unique: true
+    # add_index :customers, :unlock_token,         unique: true
   end
 end
