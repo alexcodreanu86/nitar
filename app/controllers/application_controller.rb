daa31cfc69ef0eecce782aaa61cab9eb334af564
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-05-13 11:40:41 -0500

    refactored redirect to root with flash message into unauthorized_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,20 @@ class ApplicationController < ActionController::Base
     current_user && current_user.is_admin
   end
 
+  def authorize_admin 
+    unless current_admin?
+      unauthorized_user
+    end
+  end
+
   def configure_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit :email, :password, :password_confirmation, :name, :phone
     end
+  end
+
+  def unauthorized_user
+    flash[:alert] = "You are not authorized to view this page."
+    redirect_to root_path
   end
 end
