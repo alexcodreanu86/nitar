304253376e5dfd408feb7bcf0c8d71cabfc2e6cb
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-05-12 23:20:40 -0500

    added validations for required fields

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,5 +3,5 @@ class Trip < ActiveRecord::Base
   belongs_to :user
   belongs_to :rate
 
-  validates :trip_type, :car_id, :number_of_passengers, :pickup_time, presence: true
+  validates :trip_type, :car_id, :number_of_passengers, :pickup_time, :pick_up, :drop_off, :contact_name, :contact_phone, :contact_email, presence: true
 end
