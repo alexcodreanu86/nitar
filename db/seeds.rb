580af8b7c94b9c71359089ab4eecaab515194a61
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-05-13 16:05:32 -0500

    adding admin menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,5 @@ end
 Car.create(name: "Sedan", price_ratio: 1, hourly_rate: 55, capacity: 3)
 Car.create(name: "Private Suv", price_ratio: 1.2, hourly_rate: 75, capacity: 6)
 Car.create(name: "Luxury Black Car", price_ratio: 1.3, hourly_rate: 85, capacity: 3)
-
+User.create(name: "Alex Codreanu", email: "test@test.com", password: "testtest", password_confirmation: "testtest", phone: "1231231234", is_admin: true)
 puts "Seeding completed"
