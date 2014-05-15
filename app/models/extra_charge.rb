bb8b897afe83230e4651c124a8da08ef3fd2a531
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-05-15 14:58:42 -0500

    added associations and before create and before destroy logic

---
+++ app/models/extra_charge.rb
@@ -0,0 +1,9 @@
+class ExtraCharge < ActiveRecord::Base
+  belongs_to :trip
+
+  after_create :update_trip_total_price
+  after_destroy :update_trip_total_price
+  def update_trip_total_price
+    self.trip.assign_total_price
+  end
+end
