1cf0133cdb7ddd8105d81b92fa790c6984d0ce9b
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-05-15 14:53:34 -0500

    base rate can be generated as well withoud adding the extra charges

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -2,7 +2,6 @@ class Rate < ActiveRecord::Base
 
   def self.calculate(params, ratio)
     @rate = self.where(id: params[:rate_id]).first
-
     if (1..4).cover? params[:trip_type].to_i
       @price = params[:airport].to_i == 1 ? @rate.o_hare_price : @rate.midway_price
       if params[:trip_type].to_i  == 2
@@ -13,9 +12,18 @@ class Rate < ActiveRecord::Base
     else
       @price = @rate.chicago_price
     end
-
     @price *= ratio
+    @price.to_i
+  end
 
+  def self.calculate_base(params, ratio)
+    @rate = self.where(id: params[:rate_id]).first
+    if (1..4).cover? params[:trip_type].to_i
+      @price = params[:airport].to_i == 1 ? @rate.o_hare_price : @rate.midway_price
+    else
+      @price = @rate.chicago_price
+    end
+    @price *= ratio
     @price.to_i
   end
 end
