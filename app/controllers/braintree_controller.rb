63a58c76cc02caa13e813a831bf461500a0c5b9e
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-05-22 14:21:18 -0500

    added logic to add transaction to trip

--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -27,4 +27,41 @@ class BraintreeController < ApplicationController
       redirect_to :back
     end
   end
+  
+  def new_trip_transaction
+    @trip = Trip.where(id: params[:trip_id]).first
+  end
+
+  def submit_trip_transaction
+    @trip = Trip.where(id: params[:trip_id]).first
+     
+    result = Braintree::Transaction.sale(
+      :amount => @trip.total_price,
+      :credit_card => {
+        :cardholder_name => params[:name],
+        :number => params[:number],
+        :cvv => params[:cvv],
+        :expiration_month => params[:month],
+        :expiration_year => params[:year]
+      },
+      billing: {
+        :postal_code => params[:zip]
+      },
+      :options => {
+        :submit_for_settlement =>  params[:charge]
+      }
+    )
+    binding.pry
+    if result.success?
+      @trip.transaction_id = result.transaction.id
+      @trip.payment_information = true
+      @trip.cc_last_four = result.transaction.credit_card_details.last_4
+      @trip.save
+      flash[:notice] = "Great success!"
+      redirect_to root_path
+    else
+      flash[:alert] = "Failed"
+      redirect_to :back
+    end
+  end
 end
