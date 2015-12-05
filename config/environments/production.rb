103c7732e49fb0715bf1bfa01c7956dc01a5363e
alexcodreanu86 <alexcodreanu86@gmail.com>
2015-12-05 13:57:55 -0500

    Updates for new box

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ SpartanLimo::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true 
-  config.assets.precompile = ['*.js', '*.css', '*.css.erb', '*.png', '*.jpeg'] 
+  # config.assets.precompile = ['*.js', '*.css', '*.css.erb', '*.png', '*.jpeg'] 
 
   # Generate digests for assets URLs.
   config.assets.digest = true
